2bbc87567e492862dc0f56d6d8203738dcd6670e
stoneys1 <malvesbonacina@yahoo.com.br>
2024-09-15 18:02:37 -0300

    commit 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  darkMode: 'class', // or 'media' or 'class'
+  presets: [require('./utils/tailwind-preset')],
 };
